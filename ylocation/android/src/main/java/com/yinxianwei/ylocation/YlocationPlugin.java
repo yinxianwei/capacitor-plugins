7c49fe8194fd6fdf5501c7730f39366fb986ab41
yinxianwei <yinxianwei@qq.com>
2025-08-07 10:48:17 +0800

    add huawei location

--- ylocation/android/src/main/java/com/yinxianwei/ylocation/YlocationPlugin.java
+++ ylocation/android/src/main/java/com/yinxianwei/ylocation/YlocationPlugin.java
@@ -1,8 +1,8 @@
 package com.yinxianwei.ylocation;
 
+import android.app.Activity;
 import android.location.Location;
 import android.os.Looper;
-
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
@@ -10,6 +10,7 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
 import com.huawei.hms.location.FusedLocationProviderClient;
 import com.huawei.hms.location.LocationCallback;
 import com.huawei.hms.location.LocationRequest;
@@ -23,20 +24,22 @@ public class YlocationPlugin extends Plugin {
 
     @PluginMethod
     public void getCurrentPosition(PluginCall call) {
-        this.getActivity().runOnUiThread(() -> {
-            FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
-            LocationRequest mLocationRequest = new LocationRequest();
-            Boolean enableHighAccuracy = call.getBoolean("enableHighAccuracy",  false);
-            if (enableHighAccuracy) {
-                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            }
-            Integer timeout = call.getInt("timeout", 10000);
-            mLocationRequest.setMaxWaitTime(timeout);
-            mLocationRequest.setNumUpdates(1);
-            LocationCallback  mLocationCallback = new LocationCallback() {
-                @Override
-                public void onLocationResult(LocationResult locationResult) {
+        Activity activity = getActivity();
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+        LocationRequest mLocationRequest = new LocationRequest();
+        Boolean enableHighAccuracy = call.getBoolean("enableHighAccuracy",  false);
+        if (enableHighAccuracy) {
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }
+        Integer timeout = call.getInt("timeout", 10000);
+        mLocationRequest.setMaxWaitTime(timeout);
+        mLocationRequest.setNumUpdates(1);
+        LocationCallback  mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                activity.runOnUiThread(() -> {
                     if (locationResult != null) {
+                        fusedLocationProviderClient.flushLocations();
                         Location location = locationResult.getLastLocation();
 
                         JSObject data = new JSObject();
@@ -52,17 +55,21 @@ public class YlocationPlugin extends Plugin {
 
                         call.resolve(data);
                     } else {
-                        call.reject("");
+                        call.reject("Unable to retrieve location data");
                     }
-                }
-            };
-            fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(Exception e) {
-                            call.reject(e.getMessage());
-                        }
-                    });
-        });
+                });
+            }
+        };
+        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {}
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        call.reject(e.getMessage());
+                    }
+                });
     }
 }
